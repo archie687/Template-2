5756b2bf7c595be499e45e30fc314c9f0217ba21
archie <archieparliya687@gmail.com>
2024-07-26 16:12:23 +0530

    Add existing project files to Git

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+// tailwind.config.js
+module.exports = {
+    theme: {
+      extend: {
+        screens: {
+          'xs': '475px',
+        },
+      },
+    },
+  };
+  
+  // tailwind.config.js
+// module.exports = {
+//     theme: {
+//       extend: {
+//         screens: {
+//           'range': { 'min': '640px', 'max': '767px' },
+//         },
+//       },
+//     },
+//   };
+  
